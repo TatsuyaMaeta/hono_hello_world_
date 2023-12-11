d79aa5f09cf5858d2c168542a27823aa700caf43
TatsuyaMaeta <tat.mae084@gmail.com>
2023-12-12 02:02:32 +0900

    first commit

---
+++ api/index.ts
@@ -0,0 +1,27 @@
+// import { Hono } from 'hono'
+// import { handle } from 'hono/vercel'
+
+// export const config = {
+//   runtime: 'edge',
+// }
+
+// const app = new Hono().basePath('/api')
+
+// app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
+
+// export default handle(app)
+
+import { NextRequest } from 'next/server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/api/hello', (c) => {
+  return c.json({ message: 'Hello from Hooo' })
+})
+
+export default (req) => app.request(req)
+
+export const config = {
+  runtime: 'experimental-edge',
+}
